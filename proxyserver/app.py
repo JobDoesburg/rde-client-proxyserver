b0ec69ae5f4bc22471dab9016634aa08a6aa655c
Job Doesburg <job.doesburg@surf.nl>
2022-11-15 14:08:46 +0100

    Add documentation

--- proxyserver/app.py
+++ proxyserver/app.py
@@ -1,3 +1,5 @@
+from secrets import token_urlsafe
+
 from flask import Flask, render_template
 from flask_sock import Sock
 
@@ -6,21 +8,38 @@ sock = Sock(app)
 
 connections = {}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@sock.route('/socket/<token>')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/open")
+def open_new_socket():
+    # TODO authenticate this route
+    new_token = token_urlsafe(32)
+    while new_token in connections:
+        new_token = token_urlsafe(32)
+    connections[new_token] = []
+    return new_token
+
+
+@sock.route("/socket/<token>")
 def echo(s, token):
-    if token not in connections:
-        connections[token] = []
-    connections[token].append(s)
+    if token not in connections.keys():
+        return
+
+    connections[token].append(s) # TODO only allow 2 connections per token
+
     while True:
-        data = s.receive()
+        data = s.receive() # TODO handle disconnects, timeouts, etc.
+        # TODO
         for socket in connections[token]:
             if socket != s:
                 socket.send(data)
 
 
-if __name__ == '__main__':
+# TODO: close sockets and clear connections[token]
+
+if __name__ == "__main__":
     app.run()
